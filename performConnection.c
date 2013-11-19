9e2f1427fb9c7ee11aa16efc8eca75ee46cbab19
Muahaas <fabian.ruffy@gmail.com>
2013-11-19 20:54:47 +0100

    Update performConnection.c
    Hilfsdatei, die mit dem Server kommuniziert und das Spiel initialisiert.

--- performConnection.c
+++ performConnection.c
@@ -7,11 +7,16 @@
 #include <netdb.h>
 #include <unistd.h>
 #define BUFFR 256
-#define GAMEKINDNAME "Quarto\n"
-#define VERSION "VERSION 1.0\n"
-//sscanf
+#define GAMEKINDNAME "Quarto"
+#define VERSION "VERSION 1.0"
+//Importiere Variablen
+extern char* ID;
+extern char* playerNum;
+
+
 
 //Prüfe ob der Gegenspieler im Spiel und bereit ist
+/*
 int checkPlayerReady(char* response)
 {
 
@@ -28,99 +33,171 @@ int checkPlayerReady(char* response)
     }
 
 }
-void testBuffer(char* buffer, int size) {
-int i;
-for (i=0;i<size;i++){
-
-    printf("%c\n",buffer[i]);
-
-}
-
-
-}
-
-
-int performConnection(int sock, char* gameID)
+void testBuffer(char* buffer, int size)
 {
-   // char *gameID;
-//FILE * stream = fdopen(sock, "r+b");
+    int i;
+    for (i=0; i<size; i++)
+    {
+        if (buffer[i] == '\n') printf(" \\n ");
+        else
+            printf(" %c ",buffer[i]);
 
-  //  gameID = malloc(sizeof(char)*20);
-    char* buffer = (char*) malloc(sizeof(char)*BUFFR);
-  //  strcpy(gameID,"ID  fm1y4PiVKfU");
-  //strcpy(gameID,"ID  528902d1b0074");
+    }
 
-    int size; //Return-Wert von size für Fehlerbehandlung
 
-    size = recv(sock, buffer, BUFFR-1, 0); //Empfange Server Nachrichtung und speichere sie in Buffer
-    //if (size > 0) buffer[size]='\0'; //Markiere Ende der Übertragung mit \0
-    printf("\n%s",buffer); //Ausgabe
+}
+*/
+//Bringe die zu sendende Antwort in ein für den Server angepasstes Format.
+int sendReplyFormatted(int sock, char* reply)
+{ char * container;
+   container = malloc(sizeof(char)*strlen(reply)*2); // Container-String der die Antwort übernimmt und eine Newline angehängt bekommt.
+    int err;
+    strcpy(container,reply);
+    strcat(container, "\n");
+    err = send(sock,container,strlen(container),0); //Sende den Container an den Server, err für Fehlerbehandlung.
+free(container);
+    return err;
 
-    send(sock,VERSION,strlen(VERSION),0); //Schicke Version des Spieles an Server
+}
+
+
+
+int performConnection(int sock)
+{
+
+   // gameID = malloc(sizeof(char)*20);
+    char* buffer = (char*) malloc(sizeof(char)*BUFFR); //Initialisiere den Buffer für die Kommunikation
+    //strcpy(gameID,"ID  fm1y4PiVKfU");
+    //strcpy(gameID,"ID  528902d1b0074");
+
+//Initialisiere Hilfsvariablen für die formatierte Ausgabe einer Serverantwort
+    char* reader;
+    char* readInt;
+    reader = malloc(sizeof(char)*20);
+    readInt     = malloc(sizeof(char));
+    int var1;
+
+
+    int err; //Return-Wert von err für Fehlerbehandlung
+
+    err = recv(sock, buffer, BUFFR-1, 0); //Empfange Server Nachrichtung und speichere sie in Buffer
+    sscanf(buffer, "%*s%*s%*s%s", reader);
+    printf("\nDie Version des Servers ist: %s\n", reader);
+    if (err > 0) buffer[err]='\0'; //Markiere Ende der Übertragung mit \0
+    //printf("\n%s",buffer); //Ausgabe
+
+    err = sendReplyFormatted(sock,VERSION);
+    //send(sock,VERSION,strlen(VERSION),0); //Schicke Version des Spieles an Server
 
     recv(sock, buffer, BUFFR-1, 0);
-    // if (size > 0) buffer[size]='\0';
-    printf("\n%s",buffer);
-    //gameID =strcat(gameID, "\n");
+    if (err > 0) buffer[err]='\0';
+    //printf("\n%s",buffer);
 
 
-    size =    send(sock,gameID,strlen(gameID),0); //Schicke Spiel-ID an Server
-
-
-    size = recv(sock, buffer, BUFFR-1, 0);
-    if (size > 0) buffer[size]='\0';
-    printf("\n%s",buffer);
-
-    if (strcmp(buffer,"+ PLAYING Quarto\n") !=0)   //Teste ob auch das richtige Spiel vom Server gespielt wird, falls nicht beende Verbindung
+    if (buffer[0] == '-')
     {
-        printf("Das Spiel ist nicht Quarto! Beende Verbindung.");
-        //size = send(sock,buffer,strlen(buffer),0);
-       // printf("\n%s",buffer);
+
+        printf("\nDer Server akzeptiert die Version dieses Clients nicht!\n");
+        close(sock);
+        return EXIT_FAILURE;
+    }
+    else
+    {
+
+        printf("\nDie Client-Version wurde akzeptiert, übertrage Spiel-ID...\n");
+
+
+
+    }
+
+    err = sendReplyFormatted(sock, ID); //Schicke Spiel-ID an Server
+    //err =  send(sock,gameID,strlen(gameID),0);
+
+    err = recv(sock, buffer, BUFFR-1, 0);
+
+    if (err > 0) buffer[err]='\0';
+    //printf("\n%s",buffer);
+
+
+
+    sscanf(buffer, "%*s%*s%s", reader); //Scanne Antwort nach relevanten Variablen.
+
+    if (strcmp(reader,"Quarto") !=0)   //Teste ob auch das richtige Spiel vom Server gespielt wird, falls nicht beende Verbindung
+    {
+        printf("Das Spiel, das der Server spielt, ist nicht Quarto! Beende Verbindung.");
+        // printf("\n%s",buffer);
 
         close(sock);
         return EXIT_FAILURE;
     }
     else
     {
-//Schicke Spieler Konfiguration und lese Nachrichten vom Server
-        size = recv(sock, buffer, BUFFR-1, 0);
-        if (size > 0) buffer[size]='\0';
-        printf("\nSpiel: %s",&buffer[2]); //Zeige Spielnamen an, schneide das "+" ab
+        printf("\nDer Server möchte %s spielen. Und wir auch!\n", reader);
 
-        size =  send(sock,"PLAYER\n",strlen("PLAYER\n"),0);
-
-        size = recv(sock, buffer, BUFFR-1, 0);
-        if (size > 0) buffer[size]='\0';
-
-        if (buffer[0] == '-')
-        {
-            printf("\nAlle Plaetze sind bereits belegt, versuchen sie es spaeter noch einmal!\n");
-            close(sock);
-            return EXIT_FAILURE;
-        }
-        else
-        {
-
-
-            printf("\n%s",buffer);
-        }
-
-        size = recv(sock, buffer, BUFFR-1, 0);
-        if (size > 0) buffer[size]='\0';
-        checkPlayerReady(buffer);
-        printf("\n%s",buffer);
-        do
-        {
-            send(sock,"OKWAIT\n",strlen("OKWAIT\n"),0);
-            size = recv(sock, buffer, BUFFR-1, 0);
-            if (size > 0) buffer[size]='\0';
-            printf("\n%s",buffer);
-
-        }
-        while (strcmp(buffer,"+ WAIT\n") == 0);
     }
+
+//Schicke Spieler Konfiguration und lese Nachrichten vom Server
+    err = recv(sock, buffer, BUFFR-1, 0);
+    if (err > 0) buffer[err]='\0';
+    printf("\nSpiel: %s",&buffer[2]); //Zeige Spielnamen an, schneide das "+" ab
+
+err = sendReplyFormatted(sock,playerNum);
+   // err =  send(sock,"PLAYER\n",strlen("PLAYER\n"),0);
+
+    err = recv(sock, buffer, BUFFR-1, 0);
+    if (err > 0) buffer[err]='\0';
+
+//Teste ob überhaupt noch was frei ist, falls ja gebe Name des Spielers und Nummer aus, falls nicht beende Übertragung
+    if (buffer[0] == '-')
+    {
+
+        printf("\nAlle Plaetze sind bereits belegt, versuchen sie es spaeter noch einmal!\n");
+        close(sock);
+        return EXIT_FAILURE;
+    }
+    else
+    {
+
+        sscanf(buffer, "%*s %*s %c %s",readInt, reader);
+        printf("\nDu spielst mit dem Namen %s, deine Nummer ist %s\n", reader,readInt);
+
+    }
+
+    err = recv(sock, buffer, BUFFR-1, 0);
+    if (err > 0) buffer[err]='\0';
+
+
+    sscanf(buffer, "%*s %*s %i", &var1); //Scanne Anzahl der Spieler, normalerweise immer 2
+
+    printf("\nEs spielen %i Spieler.", var1);
+    sscanf(buffer, "%*[^\n]%*s %s %s %i", readInt,reader,&var1); //Scanne Namen des Spielers und überprüfe ob dieser Bereit ist.
+    if (var1 == 0)
+    {
+        printf("\nSpieler %s mit der Nummer %s ist noch nicht bereit.\n",reader,readInt );
+    }
+    else
+    {
+        printf("\nSpieler %s mit der Nummer %s ist bereit!\n",reader,readInt );
+
+    }
+
+
+//WAIT <->OKWAIT Schleife, später zu implementieren.
+      do
+     {
+         send(sock,"OKWAIT\n",strlen("OKWAIT\n"),0);
+         err = recv(sock, buffer, BUFFR-1, 0);
+         if (err > 0) buffer[err]='\0';
+         printf("\nServer bittet zu warten.\n");
+
+     }
+     while (strcmp(buffer,"+ WAIT\n") == 0);
+
+
     free(buffer);
-    if (size < 0) perror("Fehler bei der Kommunikation:");
+    free(reader);
+    free(readInt);
+    if (err < 0) perror("Fehler bei der Kommunikation:");
     return EXIT_SUCCESS;
 
 }
